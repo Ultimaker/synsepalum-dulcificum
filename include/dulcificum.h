2dc556ab3c7801ba787760cfd5b09a30d2a6c070
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-10-26 08:46:25 +0200

    Add translation functionality to pyDulcificum
    Build pyDulcificum target
    
    Usage:
    ```bash
    cd build/<Debug/Release>
    source generate/conanrun.sh
    cd pyDulcificum
    python
    ```
    
    Then in the Python interpreter you can do:
    
    ```python
    import pyDulcificum as du
    gcode = du.read_file("<path_to_gcode_file>.gcode")  # this will simply return the file content as a string
    miracle_jtp = du.gcode_2_miracle_jtp(gcode)  # Translate the gcode to a jsontoolpath as string
    du.write_file("<path_to_jsontoolpath_file>.jsontoolpath", jsontoolpath)
    ```
    
    Contributes to CURA-10561

--- include/dulcificum.h
+++ include/dulcificum.h
@@ -5,6 +5,26 @@
 #define DULCIFICUM_VERSION "0.1.0"
 #endif
 
-// TODO: this will be the main API
+#include <dulcificum/gcode/gcode_to_command.h>
+#include <dulcificum/gcode/parse.h>
+#include <dulcificum/miracle_jtp/mgjtp_command_to_json.h>
+
+namespace dulcificum
+{
+
+[[nodiscard]] std::string GCode2Miracle_JTP(const std::string& content)
+{
+    auto gcode_ast = dulcificum::gcode::parse(content);
+    auto command_list = dulcificum::gcode::toCommand(gcode_ast);
+    auto json_commands = nlohmann::json::array();
+    for (const auto& command : command_list)
+    {
+        json_commands.emplace_back(dulcificum::miracle_jtp::toJson(*command));
+    }
+    return json_commands.dump();
+}
+
+
+} // namespace dulcificum
 
 #endif // DULCIFICUM_INCLUDE_DULCIFICUM_H
