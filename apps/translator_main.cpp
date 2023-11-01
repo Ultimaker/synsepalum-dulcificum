f09a26b1697620f897596d3b0fd3a8221267ed5a
c.lamboo <casperlamboo@gmail.com>
2023-11-01 22:29:26 +0100

    Remove unused code
    CURA-10561

--- apps/translator_main.cpp
+++ apps/translator_main.cpp
@@ -3,22 +3,9 @@
 #include <spdlog/spdlog.h>
 
 #include <docopt/docopt.h>
-#include <dulcificum/gcode/parse.h>
-#include <dulcificum/miracle_jtp/mgjtp_command_to_json.h>
-#include <dulcificum/gcode/gcode_to_command.h>
 #include <dulcificum/utils/io.h>
 #include <dulcificum.h>
 #include <map>
-
-#include <nlohmann/json.hpp>
-
-template<typename... Ts>
-struct Overload : Ts...
-{
-    using Ts::operator()...;
-};
-template<class... Ts>
-Overload(Ts...) -> Overload<Ts...>;
 
 int main(int argc, const char** argv)
 {
