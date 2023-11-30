de7fe166b2288b97dda9f0f30c750579ad4ff49c
Remco Burema <r.burema@ultimaker.com>
2023-11-30 20:35:26 +0100

    Fix deep templates futzing up MSVC.
    Now a sort of hybrid approach between compile and run time solutions.
    
    CURA-11365

--- src/gcode/ast/ast.cpp
+++ src/gcode/ast/ast.cpp
@@ -7,56 +7,65 @@
 
 namespace dulcificum::gcode::ast
 {
+    using node_likely_t = std::variant<
+        G0_G1,
+        M204,
+        M205,
+        M104,
+        M106,
+        M107,
+        M109,
+        M140,
+        M190>;
+    using node_occasional_t = std::variant<
+        G4,
+        T,
+        G90,
+        G91,
+        G92,
+        G280,
+        M82,
+        M83>;
+    using node_rare_t = std::variant<
+        Layer,
+        Mesh,
+        FeatureType,
+        InitialTemperatureBuildPlate,
+        BuildVolumeTemperature,
+        InitialTemperatureExtruder,
+        Comment>;
 
-template<class Command>
-void singleMatch(size_t line_index, const std::string& raw_line, std::optional<node_t>& result)
-{
-    if (! result)
+    template<typename N, size_t I = 0>
+    void factory_helper(size_t line_index, const std::string& raw_line, std::optional<node_t>& result)
     {
-        if (auto re_match = Command::match(raw_line))
+        if (result)
         {
-            result = std::make_optional(Command(line_index, raw_line, re_match));
+            return;
+        }
+        if constexpr (I < std::variant_size_v<N>)
+        {
+            using Command = std::variant_alternative_t<I, N>;
+            if (auto re_match = Command::match(raw_line))
+            {
+                result = Command(line_index, raw_line, re_match);
+            }
         }
     }
-}
 
-template<class... Commands>
-std::optional<node_t> match(size_t line_index, const std::string& raw_line)
-{
-    std::optional<node_t> result;
-    (singleMatch<Commands>(line_index, raw_line, result), ...);
-    return result;
-}
+    node_t factory(size_t line_index, const std::string& raw_line)
+    {
+        static const auto split_helpers = { factory_helper<node_likely_t>, factory_helper<node_occasional_t>, factory_helper<node_rare_t>, };
 
-node_t factory(size_t line_index, const std::string& raw_line)
-{
-    // Make sure to list the commands classes in order of most probable appearance in the gcode file for better performance
-    return match<
-               G0_G1,
-               M204,
-               M205,
-               M104,
-               M106,
-               M107,
-               M109,
-               M140,
-               M190,
-               G4,
-               T,
-               G90,
-               G91,
-               G92,
-               G280,
-               M82,
-               M83,
-               Layer,
-               Mesh,
-               FeatureType,
-               InitialTemperatureBuildPlate,
-               BuildVolumeTemperature,
-               InitialTemperatureExtruder,
-               Comment>(line_index, raw_line)
-        .value_or(Unknown{ line_index, raw_line });
-}
+        std::optional<node_t> result;
+        for (const auto& helper_func : split_helpers)
+        {
+            helper_func(line_index, raw_line, result);
+            if (result.has_value())
+            {
+                return result.value();
+            }
+        }
+        return Unknown{ line_index, raw_line };
+    }
 
 } // namespace dulcificum::gcode::ast
