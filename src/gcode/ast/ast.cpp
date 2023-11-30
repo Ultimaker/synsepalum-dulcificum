0587fa40637c0ac7b53b588cac576f4e5143895b
rburema <rburema@users.noreply.github.com>
2023-11-30 19:36:15 +0000

    Applied clang-format.

--- src/gcode/ast/ast.cpp
+++ src/gcode/ast/ast.cpp
@@ -7,65 +7,45 @@
 
 namespace dulcificum::gcode::ast
 {
-    using node_likely_t = std::variant<
-        G0_G1,
-        M204,
-        M205,
-        M104,
-        M106,
-        M107,
-        M109,
-        M140,
-        M190>;
-    using node_occasional_t = std::variant<
-        G4,
-        T,
-        G90,
-        G91,
-        G92,
-        G280,
-        M82,
-        M83>;
-    using node_rare_t = std::variant<
-        Layer,
-        Mesh,
-        FeatureType,
-        InitialTemperatureBuildPlate,
-        BuildVolumeTemperature,
-        InitialTemperatureExtruder,
-        Comment>;
+using node_likely_t = std::variant<G0_G1, M204, M205, M104, M106, M107, M109, M140, M190>;
+using node_occasional_t = std::variant<G4, T, G90, G91, G92, G280, M82, M83>;
+using node_rare_t = std::variant<Layer, Mesh, FeatureType, InitialTemperatureBuildPlate, BuildVolumeTemperature, InitialTemperatureExtruder, Comment>;
 
-    template<typename N, size_t I = 0>
-    void factory_helper(size_t line_index, const std::string& raw_line, std::optional<node_t>& result)
+template<typename N, size_t I = 0>
+void factory_helper(size_t line_index, const std::string& raw_line, std::optional<node_t>& result)
+{
+    if (result)
     {
-        if (result)
+        return;
+    }
+    if constexpr (I < std::variant_size_v<N>)
+    {
+        using Command = std::variant_alternative_t<I, N>;
+        if (auto re_match = Command::match(raw_line))
         {
-            return;
-        }
-        if constexpr (I < std::variant_size_v<N>)
-        {
-            using Command = std::variant_alternative_t<I, N>;
-            if (auto re_match = Command::match(raw_line))
-            {
-                result = Command(line_index, raw_line, re_match);
-            }
+            result = Command(line_index, raw_line, re_match);
         }
     }
+}
 
-    node_t factory(size_t line_index, const std::string& raw_line)
+node_t factory(size_t line_index, const std::string& raw_line)
+{
+    static const auto split_helpers = {
+        factory_helper<node_likely_t>,
+        factory_helper<node_occasional_t>,
+        factory_helper<node_rare_t>,
+    };
+
+    std::optional<node_t> result;
+    for (const auto& helper_func : split_helpers)
     {
-        static const auto split_helpers = { factory_helper<node_likely_t>, factory_helper<node_occasional_t>, factory_helper<node_rare_t>, };
-
-        std::optional<node_t> result;
-        for (const auto& helper_func : split_helpers)
+        helper_func(line_index, raw_line, result);
+        if (result.has_value())
         {
-            helper_func(line_index, raw_line, result);
-            if (result.has_value())
-            {
-                return result.value();
-            }
+            return result.value();
         }
-        return Unknown{ line_index, raw_line };
     }
+    return Unknown{ line_index, raw_line };
+}
 
 } // namespace dulcificum::gcode::ast
