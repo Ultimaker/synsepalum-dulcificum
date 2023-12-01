f37621ce4e6003e1630c6a3a937ee9636c7f3b68
Remco Burema <r.burema@ultimaker.com>
2023-12-01 09:17:05 +0100

    Fix calling next Command-template in list(s).
    A big oversight. Also turns out that it still wouldn't compile in MSVC if I did this, but fortunately if the lists are focussed on consolidating the 'easy' and 'hard' ones together it works (since the easy ones will compile, and there aren't enough hard ones to justify splitting them up further).
    
    part of CURA-11365

--- src/gcode/ast/ast.cpp
+++ src/gcode/ast/ast.cpp
@@ -7,24 +7,23 @@
 
 namespace dulcificum::gcode::ast
 {
-// Split-up templates, see below as well.
-using node_likely_t = std::variant<G0_G1, M204, M205, M104, M106, M107, M109, M140, M190>;
-using node_occasional_t = std::variant<G4, T, G90, G91, G92, G280, M82, M83>;
-using node_rare_t = std::variant<Layer, Mesh, FeatureType, InitialTemperatureBuildPlate, BuildVolumeTemperature, InitialTemperatureExtruder, Comment>;
+// Split-up templates. 'Difficult' node-types have templates with 'large' CharRangeLiterals. See below as well.
+using node_normal_t = std::variant<G0_G1, M204, M205, M104, M106, M107, M109, M140, M190, G4, T, G90, G91, G92, G280, M82, M83, Layer, Mesh, FeatureType, Comment>;
+using node_difficult_t = std::variant<InitialTemperatureBuildPlate, BuildVolumeTemperature, InitialTemperatureExtruder>;
 
 template<typename N, size_t I = 0>
 void factory_helper(size_t line_index, const std::string& raw_line, std::optional<node_t>& result)
 {
-    if (result)
-    {
-        return;
-    }
     if constexpr (I < std::variant_size_v<N>)
     {
         using Command = std::variant_alternative_t<I, N>;
         if (auto re_match = Command::match(raw_line))
         {
             result = Command(line_index, raw_line, re_match);
+        }
+        else
+        {
+            factory_helper<N, I + 1>(line_index, raw_line, result);
         }
     }
 }
@@ -34,11 +33,7 @@ node_t factory(size_t line_index, const std::string& raw_line)
     // Split-up all templates across multiple calls, in order to;
     //  1. fix an issue where in at least one compiler (MSVC) it'd not work, and,
     //  2. reduce compilation-time just a bit (still long though).
-    constexpr auto split_helpers = {
-        factory_helper<node_likely_t>,
-        factory_helper<node_occasional_t>,
-        factory_helper<node_rare_t>,
-    };
+    constexpr auto split_helpers = { factory_helper<node_normal_t>, factory_helper<node_difficult_t>, };
 
     std::optional<node_t> result;
     for (const auto& helper_func : split_helpers)
