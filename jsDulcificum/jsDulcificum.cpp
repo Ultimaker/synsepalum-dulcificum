9df019e3b356e10cee7ceb68da6e48ca8e42f449
Jelle Spijker <jelle.spijker@ultimaker.com>
2024-02-25 11:54:09 +0100

    Fix gcode_2_miracle_jtp binding.
    Removed the standalone CMakeList for jsDulcificum and incorporated it into the main CMakeLists file, streamlining the build process
     Also updated the binding of the gcode_2_miracle_jtp function in jsDulcificum.cpp to ensure the correct function is called.
    
     Contributes to NP-76

--- jsDulcificum/jsDulcificum.cpp
+++ jsDulcificum/jsDulcificum.cpp
@@ -4,9 +4,16 @@
 #include <string>
 
 using namespace emscripten;
-using namespace dulcificum;
+
+extern "C"
+{
+    const std::string gcode_2_miracle_jtp(const std::string& content)
+    {
+        return dulcificum::GCode2Miracle_JTP(content);
+    }
+}
 
 EMSCRIPTEN_BINDINGS(dulcificum)
 {
-    function("gcode_2_miracle_jtp", &GCode2Miracle_JTP);
+    function("gcode_2_miracle_jtp", &gcode_2_miracle_jtp);
 }
