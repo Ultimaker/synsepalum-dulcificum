5ce84193f1fd10709996d225af718769683c14cf
c.lamboo <casperlamboo@gmail.com>
2024-08-13 11:21:47 +0200

    Remove need for external c

--- DulcificumJS/cpp/DulcificumJS.cpp
+++ DulcificumJS/cpp/DulcificumJS.cpp
@@ -1,17 +1,7 @@
 #include <dulcificum.h>
 #include <emscripten/bind.h>
-#include <filesystem>
-#include <string>
-
-extern "C"
-{
-    const std::string gcode_2_miracle_jtp(const std::string& content)
-    {
-        return dulcificum::GCode2Miracle_JTP(content);
-    }
-}
 
 EMSCRIPTEN_BINDINGS(dulcificum)
 {
-    emscripten::function("gcode_2_miracle_jtp", &gcode_2_miracle_jtp);
+    emscripten::function("gcode_2_miracle_jtp", &dulcificum::GCode2Miracle_JTP);
 }
