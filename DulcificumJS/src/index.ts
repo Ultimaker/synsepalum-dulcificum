5cac3f55a841fdff07661553914930a4fb441c58
Simon Edwards <s.edwards@ultimaker.com>
2024-09-13 13:13:42 +0000

    Remove the Top Level Await and add `initialize()` function
    NP-389

--- DulcificumJS/src/index.ts
+++ DulcificumJS/src/index.ts
@@ -1,6 +1,16 @@
 import dulcificum, { MainModule } from './Dulcificum/dulcificum_js';
 
-const mod: MainModule = await (<any> dulcificum)({});
+let mod: MainModule = null;
+
+/**
+ * This must be executed and waited on before this module can be used.
+ */
+export async function initialize(): Promise<void> {
+  if (mod !== null) {
+    return;
+  }
+  mod = await (<any> dulcificum)({});
+}
 
 export function gcode_2_miracle_jtp(input: string): string {
   return <string> mod.gcode_2_miracle_jtp(input);
