7fd91280c58e2c7317f64e74b25f62549af06ad3
saumya.jain <saumya.jain@ultimaker.com>
2026-03-11 10:42:48 +0100

    add gcode_2_miracle_jtp_bytes binding to DulcificumJS

--- DulcificumJS/DulcificumJS.cpp
+++ DulcificumJS/DulcificumJS.cpp
@@ -1,5 +1,6 @@
 #include <dulcificum.h>
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
 
 struct info_t
 {
@@ -12,9 +13,23 @@ info_t get_info()
     return { DULCIFICUM_VERSION, DULCIFICUM_HASH };
 }
 
+emscripten::val gcode_2_miracle_jtp_bytes(const std::string& content, const size_t nb_extruders = 2)
+{
+    const auto json = dulcificum::GCode2Miracle_JTP(content, nb_extruders);
+
+    // Allocate a standalone ArrayBuffer-backed Uint8Array so callers can transfer it.
+    auto uint8_array_ctor = emscripten::val::global("Uint8Array");
+    auto out = uint8_array_ctor.new_(json.size());
+    out.call<void>("set", emscripten::val(emscripten::typed_memory_view(
+                               json.size(),
+                               reinterpret_cast<const unsigned char*>(json.data()))));
+    return out;
+}
+
 EMSCRIPTEN_BINDINGS(dulcificum)
 {
     emscripten::function("gcode_2_miracle_jtp", &dulcificum::GCode2Miracle_JTP);
+    emscripten::function("gcode_2_miracle_jtp_bytes", &gcode_2_miracle_jtp_bytes);
     // Binding for info_t structure
     emscripten::value_object<info_t>("info_t")
         .field("dulcificum_version", &info_t::dulcificum_version)
